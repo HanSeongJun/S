44d1b5fd975cf8a2bb378db7b250abc93a4eab97
한성준 <gkstjdwns2@naver.com>
2024-12-30 15:46:03 +0900

    ADD SecurityConfig.java
    사용자 정의 보안 설정 추가

---
+++ src/main/java/Spring/SpringSecurity/SecurityConfig.java
@@ -0,0 +1,23 @@
+package Spring.SpringSecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+// 사용자 정의 보안 설정
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
