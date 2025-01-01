3f09e9ee0399b382ae5b7c9f121b2634782fdc58
한성준 <gkstjdwns2@naver.com>
2025-01-01 16:07:06 +0900

    REFACTOR SecurityConfig.java, IndexController.java
    폼 인증 로그인 방식 추가

--- src/main/java/Spring/SpringSecurity/SecurityConfig.java
+++ src/main/java/Spring/SpringSecurity/SecurityConfig.java
@@ -1,15 +1,24 @@
 package Spring.SpringSecurity;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 // 사용자 정의 보안 설정
 @EnableWebSecurity // 스프링 시큐리티를 활성화, SpringSecurityFilterChain 을 생성하고 애플리케이션에 적용하여 요청을 보호.
@@ -20,31 +29,35 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(
                         auth -> auth.anyRequest().authenticated()) // 모든 요청에 대해 인증이 필요
-                .formLogin(Customizer.withDefaults()); // 폼 기반 로그인 기능 활성화
+                .formLogin(form -> form
+                        //.loginPage("/loginPage")
+                        .loginProcessingUrl("/loginProc")
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/failed")
+                        .usernameParameter("userId")
+                        .passwordParameter("passwd")
+
+                        // 인증 성공 핸들러
+                        .successHandler((request, response, authentication) -> {
+                            System.out.println("authentication: " + authentication);
+                            response.sendRedirect("/home");
+                        })
+
+                        // 인증 실패 핸들러
+                        .failureHandler((request, response, exception) -> {
+                            System.out.println("exception: " + exception.getMessage());
+                            response.sendRedirect("/login");
+                        })
+                        .permitAll()
+                ); // 폼 기반 로그인 기능 활성화
 
         return http.build();
     }
 
     @Bean // 설정 파일에서 신규 유저 생성, 우선순위는 자바 설정 파일 > yml 파일
     public UserDetailsService userDetailsService() {
-        UserDetails user = User
-                .withUsername("user")
-                .password("{noop}1111")
-                .roles("USER")
-                .build();
+        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
 
-        UserDetails user2 = User
-                .withUsername("user2")
-                .password("{noop}1111")
-                .roles("USER")
-                .build();
-
-        UserDetails user3 = User
-                .withUsername("user3")
-                .password("{noop}1111")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, user2, user3);
+        return new InMemoryUserDetailsManager(user);
     }
 }
