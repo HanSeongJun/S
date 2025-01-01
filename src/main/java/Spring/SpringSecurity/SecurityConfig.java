e6c2199295d40626f314065b2ff38ced44a6f4db
한성준 <gkstjdwns2@naver.com>
2025-01-02 02:48:58 +0900

    REFACTOR SecurityConfig.java ADD CustomAuthenticationEntryPoint.java
    httpBasic() 인증

--- src/main/java/Spring/SpringSecurity/SecurityConfig.java
+++ src/main/java/Spring/SpringSecurity/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -29,7 +30,8 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                .httpBasic(basic -> basic.
+                        authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
 
         return http.build();
     }
